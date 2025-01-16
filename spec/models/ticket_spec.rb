a79dc47f0a4384543acdb3de7f50de9c5c348270
briansteuble <bsteuble@cocc.edu>
2025-01-15 16:17:04 -0800

    Updating ticket & user specs

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+
+  let (:tick) {Ticket.new}
+
   it "exists" do
     Ticket.new
   end
+
+  it "responds to name" do
+    expect(tick).to respond_to(:name)
+  end
+
+  it "responds to description" do
+    expect(tick).to respond_to(:description)
+  end
+
+  it "responds to phone" do
+    expect(tick).to respond_to(:phone)
+  end
+
+  it "belongs to region" do
+    should belong_to(:region)
+  end
+
+  it "belongs to resource category" do
+    should belong_to(:resource_category)
+  end
+
+  it "can belong to organization" do
+    should belong_to(:organization).optional
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  let (:user) {User.new}
+
   it "exists" do
     User.new
   end
+
+  it "responds to email" do
+    expect(user).to respond_to(:email)
+  end
+
+  it "responds to role" do
+    expect(user).to respond_to(:role)
+  end
+
+  it "can belong to organization" do
+    should belong_to(:organization).optional
+  end
+
 end
