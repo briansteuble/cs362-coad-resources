7b4f02d9d73778f5c4efde335f8c5520600b8314
briansteuble <bsteuble@cocc.edu>
2025-01-25 18:33:48 -0800

    Goal 3 progress, ticket scope tests incomplete

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,22 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  let (:tick) {Ticket.new}
+  let (:ticket) {Ticket.new(id: 123)}
+
+describe "attributes tests" do
 
   it "exists" do
     Ticket.new
   end
 
   it "responds to name" do
-    expect(tick).to respond_to(:name)
+    expect(ticket).to respond_to(:name)
   end
 
   it "responds to description" do
-    expect(tick).to respond_to(:description)
+    expect(ticket).to respond_to(:description)
   end
 
   it "responds to phone" do
-    expect(tick).to respond_to(:phone)
+    expect(ticket).to respond_to(:phone)
   end
 
   it "belongs to region" do
@@ -33,3 +35,103 @@ RSpec.describe Ticket, type: :model do
   end
 
 end
+
+#validates_presence_of :name, :phone, :region_id, :resource_category_id
+#validates_length_of :name, minimum: 1, maximum: 255, on: :create
+#validates_length_of :description, maximum: 1020, on: :create
+#validates :phone, phony_plausible: true
+
+describe "validation tests" do
+
+  it "validates presence of name" do
+    expect(ticket).to validate_presence_of(:name)
+  end
+
+  it "validates presence of phone" do
+    expect(ticket).to validate_presence_of(:phone)
+  end
+
+  it "validates presence of region id" do
+    expect(ticket).to validate_presence_of(:region_id)
+  end
+
+  it "validates presence of resource category id" do
+    expect(ticket).to validate_presence_of(:resource_category_id)
+  end
+
+  it "validates name length" do
+    expect(ticket).to validate_length_of(:name).is_at_least(1)
+    expect(ticket).to validate_length_of(:name).is_at_most(255)
+  end
+
+  it "validates description length" do
+    expect(ticket).to validate_length_of(:description).is_at_most(1020)
+  end
+
+  it "validates plausible phone" do
+    should allow_value('+1-555-555-5555').for(:phone)
+  end
+
+end
+
+describe "member function tests" do
+
+  it "converts to string" do
+    expect(ticket.to_s).to eq "Ticket 123"
+  end
+
+  it "a new ticket is set to open by default" do
+    expect(ticket.open?).to eq true
+  end
+
+  it "a closed ticket is not open" do
+    ticket.closed = true
+    expect(ticket.open?).to eq false
+  end
+
+  it "is a new ticket uncaptured by default?" do
+    expect(ticket.captured?).to eq false
+  end
+
+end
+
+describe "scope tests" do
+
+  it "scopes closed tickets" do
+    region = Region.create!(name: "region1")
+    resource = ResourceCategory.create!(name: "resource1")
+
+    ticket = Ticket.create!(
+      name: "ticket",
+      phone: "+1-555-555-5555",
+      region_id: region.id,
+      resource_category_id: resource.id,
+      closed: true
+  )
+
+    expect(Ticket.closed).to include(ticket)
+    expect(Ticket.open).to_not include(ticket)
+    end
+
+  it "scopes open tickets" do
+    region = Region.create!(name: "region1")
+    resource = ResourceCategory.create!(name: "resource1")
+  
+    ticket = Ticket.create!(
+      name: "ticket",
+      phone: "+1-555-555-5555",
+      region_id: region.id,
+      resource_category_id: resource.id,
+      closed: false,
+      organization_id: nil
+  )
+  
+    expect(Ticket.closed).to_not include(ticket)
+    expect(Ticket.open).to include(ticket)  
+    end
+
+    
+  end
+
+end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let (:user) {User.new}
+  let (:user) {User.new(email: "example@gmail.com")}
+
+  describe "attributes test" do
 
   it "exists" do
     User.new
@@ -18,6 +20,57 @@ RSpec.describe User, type: :model do
 
   it "can belong to organization" do
     should belong_to(:organization).optional
+  end
+end
+
+#validates_presence_of :email
+#validates_length_of :email, minimum: 1, maximum: 255, on: :create
+#validates :email, format: { with: VALID_EMAIL_REGEX }
+#validates_uniqueness_of :email, case_sensitive: false
+#validates_presence_of :password, on: :create
+#validates_length_of :password, minimum: 7, maximum: 255, on: :create
+
+  describe "validation tests" do
+
+    it "validates presence of email" do
+      expect(user).to validate_presence_of(:email)
+    end
+
+    it "validates presence of password" do
+      expect(user).to validate_presence_of(:password)
+    end
+
+    it "validates email length" do
+      expect(user).to validate_length_of(:email).is_at_least(1)
+      expect(user).to validate_length_of(:email).is_at_most(255)
+    end
+
+    it "validates password length" do
+      expect(user).to validate_length_of(:password).is_at_least(7)
+      expect(user).to validate_length_of(:password).is_at_most(255)
+    end
+
+    it "validates uniqueness of email" do
+      expect(user).to validate_uniqueness_of(:email).case_insensitive
+    end
+
+    it "validates format of email" do
+      should allow_value('example@gmail.com').for(:email)
+    end
+
+  end
+
+  describe "member function tests" do
+
+    it "sets default user role to :organizition" do
+      user.set_default_role
+      expect(user.role).to eq("organization")
+    end
+
+    it "converts user email to string" do
+      expect(user.to_s).to eq("example@gmail.com")
+    end
+
   end
 
 end
