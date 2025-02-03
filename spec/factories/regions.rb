991f48b0d038634a8590402c733786b5484dc14e
Ethan Price <priceeth@oregonstate.edu>
2025-02-03 14:45:34 -0800

    added factory files

---
+++ spec/factories/regions.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :region do
+    name {"Region name"}
+  end
+
+end
+
+#let (:region) {FactoryBot.build_stubbed(:region)}
+#let (:region) {FactoryBot.build_stubbed(:region, name: 'asdsad')}
+#let (:region) {FactoryBot.create(:region)}  builds it in data base, required for scopes
+#nested factories one produce open ticket, one closed
